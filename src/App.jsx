4b1f6ca4e8ae7496bd3fcdc655fcfadea3e19a26
Samer A <thedev.samer@gmail.com>
2022-04-13 11:06:10 +0300

    added the not-found page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
+import NotFound from "./components/common/not-found";
 
 const App = () => {
   return (
-    <div className="app">
-      <Routes>
-        <Route path="/search/:searchTerm" element={<h1>Search</h1>} />
-        <Route path="/home" element={<h1>Home</h1>} />
-        <Route path="/not-found" element={<h1>Not Found</h1>} />
-        <Route path="/" element={<Navigate to="/home" replace />} />
-        <Route path="*" element={<Navigate to="/not-found" replace />} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/search/:searchTerm" element={<h1>Search</h1>} />
+      <Route path="/home" element={<h1>Home</h1>} />
+      <Route path="/not-found" element={<NotFound />} />
+      <Route path="/" element={<Navigate to="/home" replace />} />
+      <Route path="*" element={<Navigate to="/not-found" replace />} />
+    </Routes>
   );
 };
 

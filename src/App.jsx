f7fcbd49c4b1dfaf913e5243135fd59eb8acd554
Samer A <thedev.samer@gmail.com>
2022-04-13 13:18:05 +0300

    removed the not-found page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/home";
-import NotFound from "./components/not-found";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/not-found" element={<NotFound />} />
-      <Route path="*" element={<Navigate to="/not-found" replace />} />
+      <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
 };

9ff7ebaa5807ec480700b4b228208ad92f09ba1c
Samer A <thedev.samer@gmail.com>
2022-04-13 11:45:06 +0300

    finished building the home page header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/home";
 import NotFound from "./components/common/not-found";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/search/:searchTerm" element={<h1>Search</h1>} />
-      <Route path="/home" element={<h1>Home</h1>} />
+      <Route path="/home" element={<Home />} />
       <Route path="/not-found" element={<NotFound />} />
       <Route path="/" element={<Navigate to="/home" replace />} />
       <Route path="*" element={<Navigate to="/not-found" replace />} />
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import "./index.css";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <Header />
+
+      <div className="home__body"></div>
+    </div>
+  );
+};
+
+export default Home;

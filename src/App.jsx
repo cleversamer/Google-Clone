2f6d474585a6bfa20760098f2816f785bb26df45
Samer A <thedev.samer@gmail.com>
2022-04-13 10:53:48 +0300

    decided the app pages & added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from "react";
-import "./css/app.css";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="app">
-      <h1>Hello World!</h1>
+      <Routes>
+        <Route path="/search/:searchTerm" element={<h1>Search</h1>} />
+        <Route path="/home" element={<h1>Home</h1>} />
+        <Route path="/not-found" element={<h1>Not Found</h1>} />
+        <Route path="/" element={<Navigate to="/home" replace />} />
+        <Route path="*" element={<Navigate to="/not-found" replace />} />
+      </Routes>
     </div>
   );
 };
